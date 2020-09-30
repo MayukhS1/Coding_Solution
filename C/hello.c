86314d1f3e5eca81435ab3e9b57e4e15f1447b68
Mayukh Sasmal <49561618+MayukhS1@users.noreply.github.com>
2020-09-30 18:59:36 +0530

    Create hello.c

---
+++ C/hello.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void){
+  printf("This folder contains coding solution in c from my universiy Assignments. Nothing much... But it's honest work!!");
+  return 0;
+}
